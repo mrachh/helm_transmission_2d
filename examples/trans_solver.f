6f4ea347163cd661d3fc752f4d2159e6078363fd
mrachh <manasrachh@gmail.com>
2019-08-25 18:01:39 -0400

    adding trans solvers

---
+++ examples/trans_solver.f
@@ -0,0 +1,287 @@
+c
+c     This code solves the exterior Dirichlet problem governed by the 
+c     Helmholtz equation in 2D using Alpert quadratures.
+c    
+c
+c     We assume the representation takes the form:
+c
+c     u0 = (-1/b0) S_k0 sigma + (1/b0) D_k0 mu  (exterior)
+c
+c     u = (-1/b) S_k sigma + (1/b) D_k mu     (interior)
+c
+c     so that
+c
+c     [au] = (1/2)[a0/b0 + a/b]mu + [(a0/b0) D^*_k0 - (a/b)D^*_k] mu 
+c             - [ a0/b0 S_k0 -  a/b S_k] sigma = f1
+c     [b dudn] = [D'_k0 - D'_k] mu  + sigma - [S'_k0 - S'_k] = f2
+c
+c     or (with q = (1/2)[a0/b0+a/b] )
+c
+c     [au]/q = mu + [(a0/b0) D^*_k0 - (a/b)D^*_k]/q mu 
+c             - [ a0/b0 S_k0 -  a/b S_k]/q sigma = f1/q
+c
+      program axihelm_neu
+      implicit real *8 (a-h,o-z)
+
+      real *8, allocatable :: srcinfo(:,:)
+      complex *16, allocatable :: amattmp(:,:)
+      complex *16, allocatable :: amat(:,:),soln(:),rhs(:)
+      complex *16 zk(2),alpha,beta,ima
+      complex *16 zpars(10),ssum,ssum1,ssum2
+      complex *16 uex,u,uin,dudnin,uout,dudnout,u1,u2
+      real *8 dpars
+      integer ipars
+      real *8 targ(2,2),src(2,2)
+      complex *16 a(2),b(2),q,zz
+
+      integer, allocatable :: ipvt(:)
+      character *1 transa
+      external slp,dlp,comb,transmission_dir,transmission_neu
+
+
+      call prini(6,13)
+      pi = 4.0d0*datan(1.0d0)
+      ima = dcmplx(0.0d0,1.0d0)
+      zs = ima/4.0d0
+
+c
+c       setup sources and targets
+c       
+c       the first source/target is in the interior
+c       and the second one is in the exterior
+c
+
+      src(1,1) = 0.01d0
+      src(2,1) = -0.07d0
+
+      src(2,1) = 10.5d0
+      src(2,2) = -3.1d0
+
+      targ(1,1) = -0.02d0
+      targ(2,1) = 0.03d0
+      
+      targ(1,2) = 12.1d0
+      targ(2,2) = 5.2d0
+c
+c
+c       initialize wave speeds, and jump parameters
+c
+      zk(1) = 1.0d0
+      zk(2) = 2.0d0
+
+      a(1) = 2.0d0
+      a(2) = 1.0d0
+      b(1) = 4.0d0
+      b(2) = 3.0d0
+ 
+      nmax = 8000
+      allocate(srcinfo(5,nmax))
+c
+c       geometry params for larry cup
+c
+      zkm = max(abs(zk(1)),abs(zk(2)))
+      norder = 16
+      ppw = 20
+
+c
+c       geometry is an ellipse, major axis=a, minor axis=b
+c  
+
+c
+c      estimate length of curve 
+c
+      ninit = 200
+      a0 = 1.1d0
+      b0 = 1.3d0
+
+      rl_approx = 0
+
+      h = 2*pi/ninit
+      do i=1,ninit
+        thet = (i-1)*h
+        rl_approx = rl_approx+sqrt((a0*sin(thet))**2+
+     1     (b0*cos(thet))**2)*h
+      enddo
+
+      n = max(ceiling(rl_approx*zkm/(2*pi)*ppw),100)
+      n = 200
+
+      h = 2*pi/n
+
+c
+c       initialize the geometry
+c
+      do i=1,n
+        thet = (i-1)*h
+        srcinfo(1,i) = a0*cos(thet)
+        srcinfo(2,i) = b0*sin(thet)
+        srcinfo(5,i) = sqrt((a0*sin(thet))**2 +(b0*cos(thet))**2)
+        srcinfo(3,i) = b0*cos(thet)/srcinfo(5,i)
+        srcinfo(4,i) = a0*sin(thet)/srcinfo(5,i)
+      enddo
+      nsys = 2*n
+
+
+      allocate(amat(nsys,nsys),rhs(nsys),soln(nsys))
+      allocate(amattmp(n,n))
+c
+c
+c        the unknowns are organized as
+c    \sigma_{1},\mu_{1},\sigma_{2},\mu_{2}...
+c
+
+c
+c     form a/b sk - a0/b0 sk0 system matrix (k is interior k0 is 
+c      exterior
+c
+      zpars(1) = zk(1)
+      zpars(2) = zk(2)
+      zpars(3) = a(1)/b(1)
+      zpars(4) = -a(2)/b(2)
+      zpars(5) = 0
+      zpars(6) = 0
+      q = 0.5d0*(a(1)/b(1) + a(2)/b(2))
+      call prin2('q=*',q,2)
+      call prin2('zpars=*',zpars,12)
+      call prinf('norder=*',norder,1)
+      call prin2('h=*',h,1)
+      call formmatbac(amattmp,norder,n,srcinfo,h,transmission_dir,
+     1    dpars,zpars,ipars)
+
+      
+
+  
+      do i=1,n
+        do j=1,n
+          zz = amattmp(i,j)/q
+          amat(2*i-1,2*j-1) = zz 
+        enddo
+      enddo
+
+c
+c     form -a/b dk + a0/b0 dk0 system matrix (k is interior k0 is 
+c      exterior
+c
+      zpars(1) = zk(1)
+      zpars(2) = zk(2)
+      zpars(3) = 0
+      zpars(4) = 0
+      zpars(5) = -a(1)/b(1)
+      zpars(6) = a(2)/b(2)
+      call formmatbac(amattmp,norder,n,srcinfo,h,transmission_dir,
+     1    dpars,zpars,ipars)
+ 
+      do i=1,n
+        do j=1,n
+          amat(2*i-1,2*j) = amattmp(i,j)/q
+          write(53,*) i,j,real(amattmp(i,j)/q),imag(amattmp(i,j)/q)
+        enddo
+        amat(2*i-1,2*i) = amat(2*i-1,2*i)+1 
+      enddo
+
+c
+c     form sk' - sk0' system matrix (k is interior k0 is 
+c      exterior
+c
+      zpars(1) = zk(1)
+      zpars(2) = zk(2)
+      zpars(3) = 1
+      zpars(4) = -1
+      zpars(5) = 0
+      zpars(6) = 0
+      call formmatbac(amattmp,norder,n,srcinfo,h,transmission_neu,
+     1    dpars,zpars,ipars)
+  
+      do i=1,n
+        do j=1,n
+          write(56,*) i,j,real(amattmp(i,j)),imag(amattmp(i,j))
+          amat(2*i,2*j-1) = amattmp(i,j) 
+        enddo
+      enddo
+
+      stop
+
+c
+c     form -dk + dk0 system matrix (k is interior k0 is 
+c      exterior
+c
+      zpars(1) = zk(1)
+      zpars(2) = zk(2)
+      zpars(3) = 0
+      zpars(4) = 0
+      zpars(5) = -1
+      zpars(6) = 1
+      call formmatbac(amattmp,norder,n,srcinfo,h,transmission_neu,
+     1    dpars,zpars,ipars)
+ 
+      do i=1,n
+        do j=1,n
+          amat(2*i,2*j) = amattmp(i,j) 
+        enddo
+        amat(2*i,2*i-1) = amat(2*i,2*i-1)+1 
+      enddo
+
+c
+c       end of generating matrix
+c
+
+c
+c      now generate right hand side
+c
+
+      
+      do i=1,n
+        call slp(src(1,2),srcinfo(1,i),dpars,zk(1),ipars,uin)
+        call slp(src(1,1),srcinfo(2,i),dpars,zk(2),ipars,uout)
+        call sprime(src(1,2),srcinfo(1,i),dpars,zk(1),ipars,dudnin)
+        call sprime(src(1,1),srcinfo(2,i),dpars,zk(2),ipars,dudnout)
+
+        rhs(2*i-1) = a(2)*uout - a(1)*uin
+        rhs(2*i) = b(2)*dudnout - b(1)*dudnin
+      enddo
+
+      do i=1,nsys
+        soln(i) = rhs(i)
+      enddo
+
+      allocate(ipvt(nsys))
+      info = 0
+      call zgetrf(nsys,nsys,amat,nsys,ipvt,info)
+      
+      transa = 'n'
+      info = 0
+
+      nn = 1
+      call zgetrs(transa,nsys,nn,amat,nsys,ipvt,soln,nsys,info)
+
+c
+c     evaluate solution at target point a target point in 
+c     interior and exterior
+c
+   
+      ssum1 = 0.0d0
+      ssum2 = 0.0d0
+      do i = 1,n
+        w = srcinfo(5,i)*h
+        call slp(srcinfo(1,i),targ(1,1),dpars,zk(1),ipars,u1)
+        call dlp(srcinfo(1,i),targ(1,1),dpars,zk(1),ipars,u2)
+
+        ssum1 = ssum1 + (soln(2*i)*u2-soln(2*i-1)*u1)*w/b(1)
+
+        call slp(srcinfo(1,i),targ(1,2),dpars,zk(2),ipars,u1)
+        call dlp(srcinfo(1,i),targ(1,2),dpars,zk(2),ipars,u2)
+
+        ssum2 = ssum2 + (soln(2*i)*u2-soln(2*i-1)*u1)*w/b(2)
+      enddo
+
+      call slp(src(1,2),targ(1,1),dpars,zk(1),ipars,u1)
+      call slp(src(1,1),targ(2,1),dpars,zk(2),ipars,u2)
+
+      write(6,*) ssum1,ssum2
+      write(6,*) u1,u2
+
+
+c
+      stop
+      end
+c
--- src/helm_kernels.f
+++ src/helm_kernels.f
@@ -23,6 +23,34 @@ c
 c
 c
 c
+c
+      subroutine sprime(srcinfo,targinfo,dpars,zpars,ipars,u)
+      implicit real *8 (a-h,o-z)
+      integer ipars(*)
+      real *8 dpars(*),srcinfo(*),targinfo(*)
+      complex *16 zpars(*),u,h0,ima,zs,z,zk,h1,gx,gy
+
+      zk = zpars(1)
+      ima = dcmplx(0.0d0,1.0d0)
+      zs = ima/4.0d0
+      rr2 = (srcinfo(1)-targinfo(1))**2 + (srcinfo(2)-targinfo(2))**2
+      rr = dsqrt(rr2)
+      z = zk*rr
+      ifexpon = 1
+      call hank103(z,h0,h1,ifexpon)
+
+      gx = -zs*zk*h1*(srcinfo(1)-targinfo(1))/rr
+      gy = -zs*zk*h1*(srcinfo(2)-targinfo(2))/rr
+      
+      u = -(gx*targinfo(3) + gy*targinfo(4))
+
+      return
+      end
+c
+c
+c
+c
+c
       subroutine dlp(srcinfo,targinfo,dpars,zpars,ipars,u)
       implicit real *8 (a-h,o-z)
       integer ipars(*)
@@ -73,3 +101,147 @@ c
 
       return
       end
+c
+c
+c        transmission matrices
+c
+c
+      subroutine transmission_dir(srcinfo,targinfo,dpars,zpars,ipars,u)
+c
+c
+c         The kernel of interaction is given by
+c           alpha S_{k1} + beta S_{k2} + gamma D_{k1} + delta D_{k2}
+c         
+c          zpars(1) = k1
+c          zpars(2) = k2
+c          zpars(3:6) = alpha,beta,gamma,delta
+c          
+c
+
+      implicit real *8 (a-h,o-z)
+      integer ipars(*)
+      real *8 dpars(*),srcinfo(*),targinfo(*)
+      complex *16 zpars(*),u,h0,ima,zs,z,zk,h1,gx,gy
+      data ima/(0.0d0,1.0d0)/
+      data zs/(0.0d0,0.25d0)/
+
+      zk = zpars(1)
+      zk2 = zpars(2)
+      
+      rr2 = (srcinfo(1)-targinfo(1))**2 + (srcinfo(2)-targinfo(2))**2
+      rr = dsqrt(rr2)
+
+
+      z = zk*rr
+      ifexpon = 1
+      call hank103(z,h0,h1,ifexpon)
+
+      gx = -zs*zk*h1*(srcinfo(1)-targinfo(1))/rr
+      gy = -zs*zk*h1*(srcinfo(2)-targinfo(2))/rr
+      
+      u = zpars(5)*(gx*srcinfo(3) + gy*srcinfo(4)) + zpars(3)*zs*h0
+
+      z = zk2*rr
+      ifexpon = 1
+      call hank103(z,h0,h1,ifexpon)
+
+      gx = -zs*zk2*h1*(srcinfo(1)-targinfo(1))/rr
+      gy = -zs*zk2*h1*(srcinfo(2)-targinfo(2))/rr
+      
+      u = u+zpars(6)*(gx*srcinfo(3) + gy*srcinfo(4)) + zpars(4)*zs*h0
+
+
+      return
+      end
+c
+c
+c
+c
+c
+      subroutine transmission_neu(srcinfo,targinfo,dpars,zpars,ipars,u)
+c
+c
+c         The kernel of interaction is given by
+c           alpha S_{k1}' + beta S_{k2}' + gamma D_{k1}' + delta D_{k2}'
+c         
+c          zpars(1) = k1
+c          zpars(2) = k2
+c          zpars(3:6) = alpha,beta,gamma,delta
+c          
+c
+
+      implicit real *8 (a-h,o-z)
+      integer ipars(*)
+      real *8 dpars(*),srcinfo(*),targinfo(*)
+      complex *16 zpars(*),u,h0,ima,zs,z,zk,h1,gx,gy,h2,zk2
+      complex *16 d2gdx2,d2gdy2,d2gdxdy
+      complex *16 gd0,gs0,gd1,gs1
+      data ima/(0.0d0,1.0d0)/
+      data zs/(0.0d0,0.25d0)/
+
+      zk = zpars(1)
+      zk2 = zpars(2)
+
+      xd = targinfo(1) - srcinfo(1)
+      yd = targinfo(2) - srcinfo(2)
+      
+      rr2 = (srcinfo(1)-targinfo(1))**2 + (srcinfo(2)-targinfo(2))**2
+      rr = dsqrt(rr2)
+
+
+      z = zk*rr
+      ifexpon = 1
+      call hank103(z,h0,h1,ifexpon)
+      h2 = (2/z*h1 - h0)*zk
+
+      d2gdx2 = (-h1 + h2*xd*xd)*zk
+      d2gdxdy = h2*xd*yd*zk
+      d2gdy2 = (-h1+h2*yd*yd)*zk
+
+      gd0 = -zs*(d2gdx2*srcinfo(3)*targinfo(3) +
+     1    d2gdxdy*(srcinfo(3)*targinfo(3) + srcinfo(4)*targinfo(4)) + 
+     2    d2gdy2*srcinfo(4)*targinfo(4))
+
+      gx = zs*zk*h1*(srcinfo(1)-targinfo(1))/rr
+      gy = zs*zk*h1*(srcinfo(2)-targinfo(2))/rr
+      
+      gs0 = gx*targinfo(3) + gy*targinfo(4)
+
+
+      gd0 = 0
+
+      u = zpars(3)*gs0 + zpars(5)*gd0
+
+
+
+      z = zk2*rr
+      ifexpon = 1
+      call hank103(z,h0,h1,ifexpon)
+      h2 = (2/z*h1 - h0)*zk2
+
+      d2gdx2 = (-h1 + h2*xd*xd)*zk2
+      d2gdxdy = h2*xd*yd*zk2
+      d2gdy2 = (-h1+h2*yd*yd)*zk2
+
+      gd1 = -zs*(d2gdx2*srcinfo(3)*targinfo(3) +
+     1    d2gdxdy*(srcinfo(3)*targinfo(3) + srcinfo(4)*targinfo(4)) + 
+     2    d2gdy2*srcinfo(4)*targinfo(4))
+
+      gx = zs*zk2*h1*(srcinfo(1)-targinfo(1))/rr
+      gy = zs*zk2*h1*(srcinfo(2)-targinfo(2))/rr
+      
+      gs1 = gx*targinfo(3) + gy*targinfo(4)
+
+      gd1 = 0
+
+      u = u+zpars(4)*gs1 + zpars(6)*gd1
+
+
+
+      return
+      end
+c
+c
+c
+c
+c
