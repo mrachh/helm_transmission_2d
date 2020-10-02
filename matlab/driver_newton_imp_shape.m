eb37233d42a211dbeef889ea33efa34f05fe91bc
Carlos Borges <cecborges@gmail.com>
2020-10-02 10:34:31 -0400

    update

--- matlab/driver_newton_imp_shape.m
+++ matlab/driver_newton_imp_shape.m
@@ -474,7 +474,22 @@ end
 
 
 %results imp
-kh
+figure; hold on;
+h1 = plot(0,0);
+for ik=1:length(khv)
+    n_bd  = ceil(32*kh);
+    t_bd  = 0:2*pi/n_bd:2*pi-2*pi/n_bd;
+    var_imp = lambda_vecs(ik).coefs;
+    N_imp = (length(var_imp)-1)/2;
+    lambda_imp = (var_imp(1)+cos(bsxfun(@times,t_bd',1:N_imp))*var_imp(2:N_imp+1)'+...
+            sin(bsxfun(@times,t_bd',1:N_imp))*var_imp(N_imp+2:end)')';
+    lambda_imp_orig = lambda_imp_f(t_bd);    
+    h0 = plot(t_bd, lambda_imp_orig,'b');
+    delete(h1)        
+    h1 = plot(t_bd, lambda_imp,'r');
+    pause(1);
+    
+end
 
 
 
