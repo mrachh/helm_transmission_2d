c8c832cc917cf480eca9ee351b205534c4067519
jghoskins <jeremyhoskins@gmail.com>
2022-01-03 18:30:18 -0600

    sorry
    attached

--- matlab/cavity_codes/inverse_solver_scripts/star3_inverse_test.m
+++ matlab/cavity_codes/inverse_solver_scripts/star3_inverse_test.m
@@ -9,8 +9,8 @@ bc = [];
 bc.type = 'Dirichlet';
 bc.invtype = 'o';
 optim_opts.optim_type = 'min(gn,sd)';
-%optim_opts.eps_curv = 1e-3;
-optim_opts.filter_type = 'gauss-conv';
+%optim_opts.eps_curv = 1e-1;
+optim_opts.filter_type = 'step_length';
 opts.store_src_info = true;
-[inv_data_all2,src_info_out2] = rla.rla_inverse_solver(u_meas,bc,...
+[inv_data_all,src_info_out] = rla.rla_inverse_solver(u_meas,bc,...
                           optim_opts,opts);
