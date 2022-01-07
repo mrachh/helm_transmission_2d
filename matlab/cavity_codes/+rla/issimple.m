a863e4b2e47782096d72b4c9328dd2c48f6b6350
jghoskins <jeremyhoskins@gmail.com>
2022-01-06 22:26:57 -0600

    Update issimple.m
    aaa

--- matlab/cavity_codes/+rla/issimple.m
+++ matlab/cavity_codes/+rla/issimple.m
@@ -5,10 +5,12 @@ function out = issimple(xs,ys)
     x_big_ft = zeros([5*nn,1]);
     y_big_ft = zeros([5*nn,1]);
     x_big_ft(1:(nn/2)) = x_ft(1:(nn/2));
+    y_big_ft(1:(nn/2)) = y_ft(1:(nn/2));
     x_big_ft(5*nn -nn/2 +1:end) = x_ft((nn/2+1):end);
     y_big_ft(5*nn -nn/2 +1:end) = y_ft((nn/2+1):end);
-    xbig = real(ifft(x_big_ft));
-    ybig = real(ifft(y_big_ft));
+    xbig = real(ifft(x_big_ft))*(5);
+    ybig = real(ifft(y_big_ft))*(5);
+    %plot(xs,ys,xbig,ybig)
     
     P = polyshape(xbig,ybig);
 %     fprintf('NumRegions=%d\n',P.NumRegions)
